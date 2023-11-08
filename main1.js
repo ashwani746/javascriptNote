5ba153c8df22ad73527bf84a20e0b6ed1895a96c
Ashwani Patel <ashwanipatel29998@gmail.com>
2023-11-08 16:03:37 +0530

    forEach,Set,concat

--- main1.js
+++ main1.js
@@ -76,14 +76,14 @@ const users = [
 
 // 2. Adding New Element to the Array
 
-const arr= [1,2];
+// const arr= [1,2];
 
-const append = (arr,ele) => {
-     return[...arr,ele]
+// const append = (arr,ele) => {
+//      return[...arr,ele]
    
     // arr.push(ele);
     // return arr;
-}
+// }
 // console.log(append(arr,3));   // add in original array
 // console.log(arr)
 
@@ -93,12 +93,64 @@ const append = (arr,ele) => {
 // so i want to my original array not modyfy
 
 //the spread operator [..arr,ele]
-console.log(append(arr,4));
-console.log(arr);
+// console.log(append(arr,4));
+// console.log(arr);
 
 
 
 //  3 remove Dublicate element in the array
 
+//level 0        push(ele)
+
+// const arr= [1,2,3,3,4,5]
+// function uniqueArray(){
+//     const  uniqueElements=[];
+
+//     arr.forEach((ele) => {
+//     if(!uniqueElements.includes(ele)){
+//         uniqueElements.push(ele)
+//     }
+//     })
+//     return uniqueElements;
+// }
+// console.log(uniqueArray(arr))
 
 
+
+// level 1   set()
+// The Set object lets you store unique values of any type, whether primitive values or object references.
+// Description
+// Set objects are collections of values. A value in the set may only occur once; it is unique in the set's collection. You can iterate through the elements of a set in insertion order. The insertion order corresponds to the order in which each element was inserted into the set by the add() method successfully (that is, there wasn't an identical element already in the set when add() was called).
+
+// const arr= [1,2,3,3,4,5]
+// function uniqueArray(arr){
+//     return [...new Set(arr)]    // Set goes to array object form
+// }
+// consoleog(uniqueArray(arr))
+// console.log(arr)      // not change the original array
+
+// level 2 Reduce method
+// const arr= [1,2,3,3,4,5]
+// const UniqueArray =(arr) => {
+//             return arr.reduce((acc,ele)=>{
+//                 return acc.includes(ele) ? acc : [...acc,ele]
+//             },[])
+// }
+// console.log(UniqueArray(arr))
+
+
+// 4 Concatenating the Array           concat()
+  //  merging of two element
+
+  const mergeArray = (arr1,arr2) =>{
+    return arr1.concat(...arr2);
+
+    // return [...arr1,...arr2]    //not change in original array
+    
+    //   arr1.push(...arr2)   // have change original aray
+    //   return arr1
+  }
+  const arr1 = [4,5,6];
+  const arr2=[2,3];
+  const result = mergeArray(arr1,arr2);
+  console.log(result,arr1,arr2)
